ff18ab5e317c25a792be39a5724efec9840c6198
kingstonkoa <kingston_koa@yahoo.com>
2016-08-24 15:03:31 +0800

    have autocomplete search for philippines

---
+++ app/src/main/java/ph/edu/dlsu/unbind/DelayAutoCompleteTextView.java
@@ -0,0 +1,72 @@
+package ph.edu.dlsu.unbind;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.AttributeSet;
+import android.widget.AutoCompleteTextView;
+import android.widget.ProgressBar;
+
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+/**
+ * Created by Kingston on 8/24/2016.
+ */
+public class DelayAutoCompleteTextView extends AutoCompleteTextView {
+
+    private static final int MESSAGE_TEXT_CHANGED = 100;
+    private static final int DEFAULT_AUTOCOMPLETE_DELAY = 750;
+
+    private int mAutoCompleteDelay = DEFAULT_AUTOCOMPLETE_DELAY;
+    private ProgressBar mLoadingIndicator;
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            DelayAutoCompleteTextView.super.performFiltering((CharSequence) msg.obj, msg.arg1);
+        }
+    };
+
+    public DelayAutoCompleteTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setLoadingIndicator(ProgressBar progressBar) {
+        mLoadingIndicator = progressBar;
+    }
+
+    public void setAutoCompleteDelay(int autoCompleteDelay) {
+        mAutoCompleteDelay = autoCompleteDelay;
+    }
+
+    @Override
+    protected void performFiltering(CharSequence text, int keyCode) {
+        if (mLoadingIndicator != null) {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+        mHandler.removeMessages(MESSAGE_TEXT_CHANGED);
+        mHandler.sendMessageDelayed(mHandler.obtainMessage(MESSAGE_TEXT_CHANGED, text), mAutoCompleteDelay);
+    }
+
+    @Override
+    public void onFilterComplete(int count) {
+        if (mLoadingIndicator != null) {
+            mLoadingIndicator.setVisibility(View.GONE);
+        }
+        super.onFilterComplete(count);
+    }
+}
--- app/src/main/java/ph/edu/dlsu/unbind/MapsActivity.java
+++ app/src/main/java/ph/edu/dlsu/unbind/MapsActivity.java
@@ -7,10 +7,14 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -47,22 +51,68 @@ public class MapsActivity extends AppCompatActivity implements
     private Button searchBtn;
     private EditText searchEt;
     private int locationChangeCount = 0;
+    private Integer THRESHOLD = 2;
+    private DelayAutoCompleteTextView geo_autocomplete;
+    private ImageView geo_autocomplete_clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        geo_autocomplete_clear = (ImageView) findViewById(R.id.geo_autocomplete_clear);
+
+        geo_autocomplete = (DelayAutoCompleteTextView) findViewById(R.id.geo_autocomplete);
+        geo_autocomplete.setThreshold(THRESHOLD);
+        geo_autocomplete.setAdapter(new GeoAutoCompleteAdapter(this)); // 'this' is Activity instance
+
+        geo_autocomplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                GeoSearchResult result = (GeoSearchResult) adapterView.getItemAtPosition(position);
+                geo_autocomplete.setText(result.getAddress());
+            }
+        });
+
+        geo_autocomplete.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 0) {
+                    geo_autocomplete_clear.setVisibility(View.VISIBLE);
+                } else {
+                    geo_autocomplete_clear.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        geo_autocomplete_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                geo_autocomplete.setText("");
+            }
+        });
+
         mFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         searchBtn = (Button)findViewById(R.id.search_button);
-        searchEt = (EditText) findViewById(R.id.editText);
+        //searchEt = (EditText) findViewById(R.id.editText);
         mFragment.getMapAsync(this);
 
 
         searchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String g = searchEt.getText().toString();
+                String g = geo_autocomplete.getText().toString();
 
                 Geocoder geocoder = new Geocoder(getBaseContext());
                 List<Address> addresses = null;
